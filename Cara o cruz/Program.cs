6402644e44a86e920801415f4679c8d4daa67fe8
ne0x86 <ne0x86@users.noreply.github.com>
2018-10-06 00:34:20 +0200

    Agregar archivos de proyecto.

---
+++ Cara o cruz/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Cara_o_cruz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nCaras = 0;
+            int nCruces = 0;
+            Random generador = new Random(); 
+            Console.WriteLine("Dime cuantas tiradas quieres");
+            int tiradas = int.Parse(Console.ReadLine());
+            for (int i = 0; i < tiradas; i++) //bucle for para que se ejecute las tiradas que elija el usuario
+            {
+                int n = generador.Next(2); //generador de números del 0 al 1; 0 cruz y 1 cara
+                if (n == 1)
+                {
+                    Console.WriteLine("Ha salido cara!"); //si sale 1 muestra este mensaje y suma 1 al contador caras
+                     nCaras++;
+                }
+                else
+                {
+                    Console.WriteLine("Ha salido cruz!"); //si sale 0 muestra este mensaje y suma 1 al contador cruces
+                    nCruces++;
+                }    
+            }
+            double porcentajeCaras = nCaras*100/tiradas; //porcentaje caras
+            double porcentajeCruz =nCruces*100/tiradas; //porcentaje cruces
+            Console.WriteLine("El número de caras es " + nCaras + " y el porcentaje ha sido " + porcentajeCaras+"%");
+            Console.WriteLine("El número de cruces es " + nCruces +" y el porcentaje ha sido " + porcentajeCruz+"%");
+        }
+    }
+}
